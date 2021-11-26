03d9fdf3f8e73ec62818322ae76550974ed7a545
Núbia Segóvia <rsegovia.nubia@gmail.com>
2021-11-25 22:49:49 -0300

    [ADD] cards initial structure

--- src/components/structure/Card/Card.js
+++ src/components/structure/Card/Card.js
@@ -1,1 +1,41 @@
-import { useNavigate } from 
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
+
+function Card(props) { 
+
+    const navigate = useNavigate();
+
+    const goToGamePage = () => {
+        navigate('/game', { state: props.id })
+      }
+
+    const wishGame = () => {
+        const token = localStorage.token;
+        const config = {
+            headers: { Authorization: `Bearer ${token} `}
+        }
+        const id = props.id;
+
+        axios.get(`/game/wish/${id}`, config)
+    }
+
+    return (
+        <div className='card'>
+
+        <div className='card-item' onClick={goToGamePage}>
+          <div className='card-image'>
+            <img src={props.image} alt={props.title}></img>
+          </div>
+          <h2 className='card-title'>{props.title}</h2>
+          <span className='card-preco'>{'R$ ' + props.preco}</span>
+        </div>
+  
+        <button className='wishlist' onClick={wishGame}>
+          <AiOutlineHeart color="red" />
+        </button>
+      </div>
+    )
+}
+
+export default Card;
